0822e62908661ec5a503f24b6d4bd270a1747d33
madebydavid <sutherland.dave@gmail.com>
2013-11-26 16:04:42 +0000

    basic wordpress skeleton structure

---
+++ local-config-sample.php
@@ -0,0 +1,14 @@
+<?php
+/*
+This is a sample local-config.php file
+In it, you *must* include the four main database defines
+
+You may include other settings here that you only want enabled on your local development checkouts
+*/
+
+define( 'DB_NAME', 'DiverseDigital' );
+define( 'DB_USER', 'root' );
+define( 'DB_PASSWORD', '' );
+define( 'DB_HOST', 'localhost' ); // Probably 'localhost'
+define('WP_HOME', 'http://local.diversedigital.org');
+define('WP_SITEURL', 'http://local.diversedigital.org');
