28b3e5b35f9b4051689fb896d1afa610412f2914
madebydavid <sutherland.dave@gmail.com>
2013-11-27 17:36:49 +0000

    salts must be empty

--- wp-config.php
+++ wp-config.php
@@ -31,14 +31,14 @@ define( 'DB_COLLATE', '' );
 // Salts, for security
 // Grab these from: https://api.wordpress.org/secret-key/1.1/salt
 // ==============================================================
-define('AUTH_KEY',         '+?!OB^vr nj3]Q_bUBRBUgl%q9kux.KkmJc@q;3Tv8f]g9]/5 |Y=5MC#B|9TbK ');
-define('SECURE_AUTH_KEY',  'q9Am|Yk)]?PF0=p%n/ab~%+[Il{8+D YT,B%[%/JbK,`kQ9Z##`jaMkun$kq7qgg');
-define('LOGGED_IN_KEY',    '@>y/@MISY_q3w- !2<Km*C>Ny8=]htFhxCKIzrjCf7NBgH@>=PPgbb<h3rPBTo`i');
-define('NONCE_KEY',        '&+hHD*aZgyEi7$0&Bc]+X)``7I~;HKajduUU+-f%d`obk+BWlE`/_OSpT;Oo^{i!');
-define('AUTH_SALT',        '3vdc/NzbDrSi8.MbkZI%H<|B{qz!UtPYim}CAf^:2o2G,h#rCdwLR[9QY<)yNH);');
-define('SECURE_AUTH_SALT', 'KdTI2PaB[4X/=FzLP;8w[UZRW#_7-x-+iO[YsZ-[TFn6zBqKK#NxB fvwCJA$|xm');
-define('LOGGED_IN_SALT',   'T5L4Tsy@/ w|)eD[1yvq!fA/v.1bjW}iBqHXFv$%.~0ONzQS+.a?E?tBXP@|H=+Q');
-define('NONCE_SALT',       'i!8[eT>1)[c7jBz~7)3%dH~$[H4bY-CslW, dBna^DwhBR=mjP;b+`7@_6Tn+gKI');
+define( 'AUTH_KEY',         'put your unique phrase here' );
+define( 'SECURE_AUTH_KEY',  'put your unique phrase here' );
+define( 'LOGGED_IN_KEY',    'put your unique phrase here' );
+define( 'NONCE_KEY',        'put your unique phrase here' );
+define( 'AUTH_SALT',        'put your unique phrase here' );
+define( 'SECURE_AUTH_SALT', 'put your unique phrase here' );
+define( 'LOGGED_IN_SALT',   'put your unique phrase here' );
+define( 'NONCE_SALT',       'put your unique phrase here' );
 
 // ==============================================================
 // Table prefix
